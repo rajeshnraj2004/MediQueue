480b00a93a5e5af4a2622774181101d8b3f7a48f
Rajesh N <rajeshnraj2004@gmail.com>
2026-05-16 09:37:31 +0530

    New Version

---
+++ Apps/Patient-App/app/screens/MedicalRecordsSreeen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MedicalRecordsSreeen = () => {
+  return (
+    <View>
+      <Text>MedicalRecordsSreeen</Text>
+    </View>
+  )
+}
+
+export default MedicalRecordsSreeen
